52059615b7d54fc9608cad8b02308ae479a0fe6d
Mohamed-Ugas <136655335+Mohamed-Ugas@users.noreply.github.com>
2023-08-01 11:45:37 +0100

    put the question objects inside an array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,18 +1,25 @@
+// We hav a variable "questions" with an array of objects and each object has three properties: the questions, answers that the user can choose from and the actual right answer.
 
-var questionONe = {
+var questions = [
+{
     questionText: "Commonly used data types DO Not Include:",
     answers: ["1. strings, 2. booleans, 3. alerts, 4. numbers"],
     rightAnswer: "2. booleans"
-};
+},
 
-var questionTwo = {
+{
     questiontext: "The condition in an if/ else statement is enclosed with.....",
     answers: ["1. quotes, 2. curly brackets, 3. parenthesis, 4. square brackets"],
     rightAnswer: "3. parenthesis"
-};
+},
 
-var questionThree = {
+{
     questionText: "Arrays in JavaScript can be used to store...",
     answers: ["1. numbers and strings, 2. other arrays, 3. booleans, 4. all of the above"],
     rightAnswer: "4. all of the above" 
-};
+},
+
+]
+
+// This variable is to keep track which question we are showing the user
+var currentQuestion = 0;

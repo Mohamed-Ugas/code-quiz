e1dded4c7c8bdc04b29c8bb0c42beeb0bbaaa1bb
Mohamed-Ugas <136655335+Mohamed-Ugas@users.noreply.github.com>
2023-08-01 11:28:04 +0100

    added three questions

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+
+var questionONe = {
+    questionText: "Commonly used data types DO Not Include:",
+    answers: ["1. strings, 2. booleans, 3. alerts, 4. numbers"],
+    rightAnswer: "2. booleans"
+};
+
+var questionTwo = {
+    questiontext: "The condition in an if/ else statement is enclosed with.....",
+    answers: ["1. quotes, 2. curly brackets, 3. parenthesis, 4. square brackets"],
+    rightAnswer: "3. parenthesis"
+};
+
+var questionThree = {
+    questionText: "Arrays in JavaScript can be used to store...",
+    answers: ["1. numbers and strings, 2. other arrays, 3. booleans, 4. all of the above"],
+    rightAnswer: "4. all of the above" 
+};
